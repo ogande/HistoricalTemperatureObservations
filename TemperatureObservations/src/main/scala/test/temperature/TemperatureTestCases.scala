c1e9f101f9e02381ae873e7a0089a495fb4edc6c
ogande <56527527+ogande@users.noreply.github.com>
2019-10-21 11:28:45 +0530

    Initial commit

---
+++ TemperatureObservations/src/main/scala/test/temperature/TemperatureTestCases.scala
@@ -0,0 +1,14 @@
+package main.scala.test.temperature
+
+// Defining a class to write the test cases to test the temperature data
+class TemperatureTestCases {
+  /* Description: Method to match the count before and after processing
+     Parameters: Unit
+     Return type: Boolean */    
+  def countMatching:Boolean = {true} // Count matching before after processing
+  
+  /* Description: Method to match the count before and after processing for the given year
+   	 Parameters: year of Int type
+   	 Return type: Boolean */
+  def countMatchingOfAGivenyear(year:Int):Boolean = {true} // count validation of a given year's data
+}
